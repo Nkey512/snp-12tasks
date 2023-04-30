479ab948ea58dafe9ab20e8073bd20009e73bdb2
Nikita <53521100+Nkey512@users.noreply.github.com>
2023-04-30 13:27:22 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,6 +1,16 @@
+class WrongNumberOfPlayersError(Exception):
+    """Raised when number of players is not two"""
+    pass
+
+
+class NoSuchStrategyError(Exception):
+    """Raised when move is not correct"""
+    pass
+
+
 def rps_game_winner(moves):
     if len(moves) != 2:
-        raise Exception('WrongNumberOfPlayersError')
+        raise WrongNumberOfPlayersError('Number of players must be equal 2')
 
     allowed_moves = ('R', 'P', 'S')
 
@@ -8,7 +18,7 @@ def rps_game_winner(moves):
     p2_move = moves[1][1]
 
     if p1_move not in allowed_moves or p2_move not in allowed_moves:
-        raise Exception('NoSuchStrategyError')
+        raise NoSuchStrategyError('Moves must be "R", "S" or "P"')
 
     if p1_move == p2_move:
         return ' '.join(moves[0])
